02ab27ac76f78532e6f2133c99cdf9b012fe33c7
Mahdi Pedramrazi <mpedramrazi@ebay.com>
2014-06-21 14:57:24 -0700

    adding env variables for host and port

--- app.js
+++ app.js
@@ -193,8 +193,8 @@ if(!process.env.NODE_ENV){
 }
 
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 3000
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var server_port =  process.env.PORT || 3000;
+var server_ip_address = process.env.HOST || '127.0.0.1';
  
 
 if(!app.get('port')){
